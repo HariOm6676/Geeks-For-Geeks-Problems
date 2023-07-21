e0b142078902521498e7177be0f863d08c551e55
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2023-07-22 00:14:45 +0530

    Added solution - LeetHub

---
+++ Reverse a Linked List in groups of given size - GFG/reverse-a-linked-list-in-groups-of-given-size.py
@@ -0,0 +1,83 @@
+"""Return reference of new head of the reverse linked list
+  The input list will have at least one element
+  Node is defined as
+
+class Node:
+    def __init__(self, data):
+		self.data = data
+		self.next = None
+  This is method only submission.
+  You only need to complete the method.
+"""
+class Solution:
+    def reverse(self,head, k):
+        # Code here
+        l=[]
+        temp=head
+        l1=[]
+        while temp:
+            l.append(temp.data)
+            temp=temp.next
+        for i in range(0,len(l),k):
+            l2=l[i:i+k]
+            l1.extend(l2[::-1])
+            
+        # print(l1)
+        i=0
+        head1=head
+        # t1=head1
+        while head1:
+            head1.data=l1[i]
+            # print(head1.data)
+            head1=head1.next
+            i+=1
+        return head
+        
+
+
+#{ 
+ # Driver Code Starts
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        # self.tail
+
+    def push(self, new_data):
+        new_node = Node(new_data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def printList(self):
+        temp = self.head
+        while (temp):
+            print(temp.data, end=" ")
+            # arr.append(str(temp.data))
+            temp = temp.next
+        print()
+
+if __name__ == '__main__':
+    t = int(input())
+    while (t > 0):
+        llist = LinkedList()
+        n = input()
+        values = list(map(int, input().split()))
+        for i in reversed(values):
+            llist.push(i)
+        k = int(input())
+        new_head = LinkedList()
+        ob=Solution()
+        new_head = ob.reverse(llist.head, k)
+        llist.head = new_head
+        llist.printList()
+        t -= 1
+
+
+
+
+# } Driver Code Ends
