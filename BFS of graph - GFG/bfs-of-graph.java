ca706c500264d46b42cd9b2db59628288f48e1bb
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2023-10-18 14:55:09 +0530

    Added solution - LeetHub

---
+++ BFS of graph - GFG/bfs-of-graph.java
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+// Initial Template for Java
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine().trim());
+        while (T-- > 0) {
+            String[] s = br.readLine().trim().split(" ");
+            int V = Integer.parseInt(s[0]);
+            int E = Integer.parseInt(s[1]);
+            ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+            for (int i = 0; i < V; i++) adj.add(i, new ArrayList<Integer>());
+            for (int i = 0; i < E; i++) {
+                String[] S = br.readLine().trim().split(" ");
+                int u = Integer.parseInt(S[0]);
+                int v = Integer.parseInt(S[1]);
+                adj.get(u).add(v);
+                // adj.get(v).add(u);
+            }
+            Solution obj = new Solution();
+            ArrayList<Integer> ans = obj.bfsOfGraph(V, adj);
+            for (int i = 0; i < ans.size(); i++)
+                System.out.print(ans.get(i) + " ");
+            System.out.println();
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        Queue<Integer> q= new LinkedList<>();
+        ArrayList<Integer> a= new ArrayList<Integer>();
+        Set<Integer> s= new HashSet<>();
+        q.add(0);
+        s.add(0);
+        while(!q.isEmpty())
+        {
+            int t=q.poll();
+
+            a.add(t);
+            for (int j=0;j<adj.get(t).size();j++)
+            {
+                if(!s.contains(adj.get(t).get(j)))
+                {
+                    s.add(adj.get(t).get(j));
+                    q.add(adj.get(t).get(j));
+                    
+                }
+            }
+        }
+        return a;
+    }
+}
